4bcb6e3d65a57958ebf2371cead991c09b59c79d
Artem <artem7mag@gmail.com>
2018-12-27 10:59:54 +0200

    refactored PreOAuth2SSOGatewayFilter for safety work with non secure path

--- src/main/java/org/c4isr/delta/cloudgateway/filter/PreOauth2SSOGatewayFilter.java
+++ src/main/java/org/c4isr/delta/cloudgateway/filter/PreOauth2SSOGatewayFilter.java
@@ -1,7 +1,8 @@
 package org.c4isr.delta.cloudgateway.filter;
 
 import org.c4isr.delta.cloudgateway.jwt.JwtOAuth2User;
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,8 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 
 @Component("Oauth2SSOGatewayFilterFactory")
@@ -24,24 +27,26 @@ public class PreOauth2SSOGatewayFilter extends AbstractGatewayFilterFactory<PreO
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange,chain) -> ReactiveSecurityContextHolder.getContext()
-                  .map(securityContext -> securityContext.getAuthentication())
-                  .map(authentication -> (OAuth2AuthenticationToken) authentication)
-                  .map(oAuth2Authentication -> oAuth2Authentication.getPrincipal())
-                  .map(o -> (JwtOAuth2User) o)
-                  .map(jwtOAuth2User -> jwtOAuth2User.getJwtTokenValue())
-                  .flatMap(bearerToken -> {
-          ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
-          builder.header(HttpHeaders.AUTHORIZATION,"Bearer "+bearerToken);
-          ServerHttpRequest request = builder.build();
-          return chain.filter(exchange.mutate().request(request).build());
-      });
+        return (exchange, chain) -> ReactiveSecurityContextHolder.getContext()
+                .filter(Objects::nonNull)
+                .map(securityContext -> securityContext.getAuthentication())
+                .filter(authentication -> authentication instanceof OAuth2AuthenticationToken)
+                .map(authentication -> (OAuth2AuthenticationToken) authentication)
+                .map(oAuth2Authentication -> oAuth2Authentication.getPrincipal())
+                .filter(oAuth2User -> Objects.nonNull(oAuth2User) && oAuth2User instanceof JwtOAuth2User)
+                .map(o -> (JwtOAuth2User) o)
+                .map(jwtOAuth2User -> jwtOAuth2User.getJwtTokenValue())
+                .map(bearerToken -> {
+                    ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
+                    builder.header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken);
+                    ServerHttpRequest request = builder.build();
+                    return exchange.mutate().request(request).build();})
+                .defaultIfEmpty(exchange)
+                .flatMap(chain::filter);
     }
 
 
-
-
-    public static class Config{
+    public static class Config {
 
     }
 
